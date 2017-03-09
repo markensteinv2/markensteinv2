b4d72b641eb9534d75e66887a1d55d68c995cd6b
markensteinv2 <heyimezekiel@gmail.com>
2017-03-09 19:43:13 +0800

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,117 @@
+<!DOCTYPE HTML>
+<!--
+	Lens by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Explore</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-loading-0 is-loading-1 is-loading-2">
+
+		<!-- Main -->
+			<div id="main">
+
+				<!-- Header -->
+					<header id="header">
+						<h1>Moments</h1>
+						<p>Every moments is precious to those who know the value of time.</p>
+						<ul class="icons">
+							<li><a href="http://twitter.com/kendrew_grey" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="http://instagram.com/kendrew_grey" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="http://facebook.com/markensteinv2" class="icon fa-facebook"><span class="label">Github</span></a></li>
+							<li><a href="mailto:heyimezekiel@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+						</ul>
+					</header>
+
+				<!-- Thumbnail -->
+					<section id="thumbnails">
+						<article>
+							<a class="thumbnail" href="https://lasrra.bn1304.livefilestore.com/y3mRcM1277Dw5zQtPdA3HmGesYtbwigrOWTnVXdTjK9NF0LIOifUZ9WYw0eSFr7c-du2VVHdl3jKZWmOyyBDpTbo_mlR1UddyzJOuHjVo3yPll_aktxQE3DBqgFgSA80wCR2LcX7N4PMdQU9tHTYXGW-XgOYKeMYU8nnyg4eCYq06M?width=4608&height=3456&cropmode=none" data-position="left center"><img src="https://lasrra.bn1304.livefilestore.com/y3mRcM1277Dw5zQtPdA3HmGesYtbwigrOWTnVXdTjK9NF0LIOifUZ9WYw0eSFr7c-du2VVHdl3jKZWmOyyBDpTbo_mlR1UddyzJOuHjVo3yPll_aktxQE3DBqgFgSA80wCR2LcX7N4PMdQU9tHTYXGW-XgOYKeMYU8nnyg4eCYq06M?width=4608&height=3456&cropmode=none" alt="" /></a>
+							<h2>Osmeña Peak</h2>
+							<h6>Osmeña Peak is the highest point in the island of Cebu, a province in the Philippines. Standing 1,013 meters above sea level, this stunning site offers a dramatic view of jagged cliffs with a view of the sea in the distance.</h6>
+						</article>
+
+						<article>
+							<a class="thumbnail" href="https://lqsrra.bn1304.livefilestore.com/y3mQ9Wd6ekz-OSNNlxMvW5uJlG35i8WQFZYgIi0XV-JgoCCRofnhYTKRPEJTuddX1MuvUc-kcQgjBn5GFLvXAGmc-J3r4RSaukPgizkTdMHtC5R_zGK_cV_Z216SjXge1YgppXcI5spvaMpKu6QY2nnhO6Wcc35yTOk8p5IJfFe9xI?width=1024&height=830&cropmode=none"><img src="https://lqsrra.bn1304.livefilestore.com/y3mQ9Wd6ekz-OSNNlxMvW5uJlG35i8WQFZYgIi0XV-JgoCCRofnhYTKRPEJTuddX1MuvUc-kcQgjBn5GFLvXAGmc-J3r4RSaukPgizkTdMHtC5R_zGK_cV_Z216SjXge1YgppXcI5spvaMpKu6QY2nnhO6Wcc35yTOk8p5IJfFe9xI?width=1024&height=830&cropmode=none" alt="" /></a>
+							<h2>Mantayupan Falls</h2>
+							<h6>Mantayupan Falls is a famous natural waterfalls resort located in Barili Municipality, Cebu Province, Philippines. Mantayupan Waterfall, one of the top tourist attractions in this municipality, is approximately 200 feet high from top to bottom water levels. </h6>
+						</article>
+						
+						<article>
+							<a class="thumbnail" href="images/fulls/03.jpg" data-position="top center"><img src="images/thumbs/03.jpg" alt="" /></a>
+							<h2>Nec accumsan enim felis</h2>
+							<p>Maecenas eleifend tellus ut turpis eleifend, vitae pretium faucibus.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/04.jpg"><img src="images/thumbs/04.jpg" alt="" /></a>
+							<h2>Donec maximus nisi eget</h2>
+							<p>Tristique in nulla vel congue. Sed sociis natoque parturient nascetur.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/05.jpg" data-position="top center"><img src="images/thumbs/05.jpg" alt="" /></a>
+							<h2>Nullam vitae nunc vulputate</h2>
+							<p>In pellentesque cursus velit id posuere. Donec vehicula nulla.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/06.jpg"><img src="images/thumbs/06.jpg" alt="" /></a>
+							<h2>Phasellus magna faucibus</h2>
+							<p>Nulla dignissim libero maximus tellus varius dictum ut posuere magna.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/07.jpg"><img src="images/thumbs/07.jpg" alt="" /></a>
+							<h2>Proin quis mauris</h2>
+							<p>Etiam ultricies, lorem quis efficitur porttitor, facilisis ante orci urna.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/08.jpg"><img src="images/thumbs/08.jpg" alt="" /></a>
+							<h2>Gravida quis varius enim</h2>
+							<p>Nunc egestas congue lorem. Nullam dictum placerat ex sapien tortor mattis.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/09.jpg"><img src="images/thumbs/09.jpg" alt="" /></a>
+							<h2>Morbi eget vitae adipiscing</h2>
+							<p>In quis vulputate dui. Maecenas metus elit, dictum praesent lacinia lacus.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/10.jpg"><img src="images/thumbs/10.jpg" alt="" /></a>
+							<h2>Habitant tristique senectus</h2>
+							<p>Vestibulum ante ipsum primis in faucibus orci luctus ac tincidunt dolor.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/11.jpg"><img src="images/thumbs/11.jpg" alt="" /></a>
+							<h2>Pharetra ex non faucibus</h2>
+							<p>Ut sed magna euismod leo laoreet congue. Fusce congue enim ultricies.</p>
+						</article>
+						<article>
+							<a class="thumbnail" href="images/fulls/12.jpg"><img src="images/thumbs/12.jpg" alt="" /></a>
+							<h2>Mattis lorem sodales</h2>
+							<p>Feugiat auctor leo massa, nec vestibulum nisl erat faucibus, rutrum nulla.</p>
+						</article>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; 2017 Markenstein.</li><li>Design: HTML5 UP</a>.</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
